b6377b46a4a726af4bdc764081d6f2b745f23154
c3i-user2 <swaminat_jr@yahoo.com>
2020-02-04 10:09:34 +0530

    v24

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v23!";
+        return "Hello World - v24!";
     }
 
     public static void main(String[] args) throws Exception {
