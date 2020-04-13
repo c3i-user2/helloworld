857207a6bb4808c554980b5e56b83bd0241fd582
c3i-user2 <60143758+c3i-user2@users.noreply.github.com>
2020-04-13 14:58:47 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v25!";
+        return "Hello World - v26!";
     }
 
     public static void main(String[] args) throws Exception {
