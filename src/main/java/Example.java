81c1ab9446158a4b9606528bef8c9d0b2f7c9309
c3i-user2 <60143758+c3i-user2@users.noreply.github.com>
2020-04-13 17:47:55 +0530

    v28
    version 28

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v27!";
+        return "Hello World - v28!";
     }
 
     public static void main(String[] args) throws Exception {
